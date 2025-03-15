eb2046a1df52cecef24838c7d35cc3e281dab4c8
Jabir Nahari <Jabir.Nahari@outlook.com>
2025-03-15 09:10:53 +0300

    ahhh

--- control_component/log_pump.py
+++ control_component/log_pump.py
@@ -1,8 +1,11 @@
 from pump_controller import get_status
 import time
 def log():
-    with open("../logs/pump_log.txt", "x") as file:
-        file.write("Date/Time, Status\n")
+    try:
+        with open("../logs/pump_log.txt", "x") as file:
+            file.write("Date/Time, Status\n")
+    except FileExistsError:
+        print("File exists logging is executed")
     with open('../logs/pump_log.txt', "a") as file:
         while True:
             file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')}, {get_status()}\n")
