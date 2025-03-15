50e89c90b6f969fdd35f7dabffd3618152ec8254
Jabir Nahari <Jabir.Nahari@outlook.com>
2025-03-15 08:49:41 +0300

    again

--- control_component/log_pump.py
+++ control_component/log_pump.py
@@ -2,7 +2,7 @@ import pump_controller
 import time
 def log():
     
-    with open('./logs/pump_log.txt', "a") as file:
+    with open('../logs/pump_log.txt', "a") as file:
         while True:
             file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Pump {pump_controller.get_status}\n")
             time.sleep(1)
