b0091ee1613b1114fb14d046f6df4d9835928b2b
Jabir Nahari <Jabir.Nahari@outlook.com>
2025-03-15 09:21:28 +0300

    Formatting logs

--- control_component/log_pump.py
+++ control_component/log_pump.py
@@ -3,12 +3,12 @@ import time
 def log():
     try:
         with open("../logs/pump_log.txt", "x") as file:
-            file.write("Date/Time, Status\n")
+            file.write("Date/Time,            Status\n")
     except FileExistsError:
         print("File exists logging is executed")
     with open('../logs/pump_log.txt', "a") as file:
         while True:
-            file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')}, {get_status()}\n")
+            file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')},  {get_status()}\n")
             time.sleep(1)
             
 log()
