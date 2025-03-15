b6ff186989d2c74ce0d331697033b9f50e496be4
Jabir Nahari <Jabir.Nahari@outlook.com>
2025-03-15 08:41:15 +0300

    started logging

--- control_component/pump_controller.py
+++ control_component/pump_controller.py
@@ -2,8 +2,25 @@ import RPi.GPIO as GPIO
 import timer as timer
 
 insulin_dose = input("Please enter the dosage: ")
+pump_flow_rate = 0.0028 # micro liters Per second
 
-def calculate_dose_time(dose):
-    dose_time = dose+1;
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+used_pin = 24
+
+
+def calculate_dose_time(dose): # Dose is amount of micro liters
+    dose_time = dose*pump_flow_rate;
     return dose_time
+
+def pump():
+    GPIO.setup(used_pin, GPIO.OUT)
+    GPIO.output(used_pin, GPIO.HIGH)
+    timer.wait(calculate_dose_time(insulin_dose))
+    GPIO.output(used_pin, GPIO.LOW)
+    
+
+def get_status():
+    GPIO.setup(used_pin, GPIO.IN)
+    return GPIO.input(used_pin)
 
--- tests/run_stop_pump.py
+++ tests/run_stop_pump.py
@@ -1,13 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 breaker_pin = 24
-GPIO.setwarnings(False)
 GPIO.setup(breaker_pin, GPIO.OUT)
 
-GPIO.output(breaker_pin, GPIO.LOW)
 command = int(input("Enter 1 to run and 2 to stop: "))
 def control_breaker():
     if command == 1:
