58773c9b325b1de748634a8d7a0a9c6c2347e269
Jabir-Nahari <Jabir.Nahari@outlook.com>
2025-04-19 01:32:38 +0300

    Fix test

--- tests/adl_test.py
+++ tests/adl_test.py
@@ -5,10 +5,10 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 # Create the I2C bus
-i2c = busio.I2C(board.GP5, board.GP3)
+i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
-ads = ADS.ADS1115(i2c)
+ads = ADS.ADS1115(i2c, address =0x36)
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
