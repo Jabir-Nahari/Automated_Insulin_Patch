bc099c5b5ad98f063503e5c5bf947ceb4f783680
Jabir-Nahari <Jabir.Nahari@outlook.com>
2025-04-19 01:28:40 +0300

    Fix test

--- tests/adl_test.py
+++ tests/adl_test.py
@@ -5,7 +5,7 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 # Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.GP5, board.GP3)
 
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
