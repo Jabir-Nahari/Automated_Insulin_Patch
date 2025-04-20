b7a244396a1c7b901e1440e893031f095e59d757
Jabir-Nahari <Jabir.Nahari@outlook.com>
2025-04-20 05:47:26 +0300

    Temprature_sensor_controller

--- control_component/run.py
+++ control_component/run.py
@@ -1,7 +1,7 @@
 import temperature_sensor_controller as tsc
 import asyncio
 async def main():
-    temp_controller = tsc.temprature_sensor(35)
+    temp_controller = tsc.TemperatureSensor(35)
     task1 = asyncio.create_task(temp_controller.temprature_check())
     task2 = asyncio.create_task(temp_controller.store_temprature())
     await asyncio.wait([task1, task2],timeout=10,return_when=asyncio.ALL_COMPLETED)
