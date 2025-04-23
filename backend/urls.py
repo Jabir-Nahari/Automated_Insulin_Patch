16def3c3366868e7e967b28abf3ccb6bb9313578
Jabir-Nahari <Jabir.Nahari@outlook.com>
2025-04-23 08:57:52 +0300

    added backend, views, model...

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from . import views  # Import your app's views
 
 urlpatterns = [
     # Define your app's URL patterns here
-    path('#schedule', views.scheduling_api, name='scheduling'),
+    path('api/schedule/', views.scheduling_api, name='scheduling'),
     path('', views.main_page, name='main_page'),
     # Add more paths as needed
 ]
